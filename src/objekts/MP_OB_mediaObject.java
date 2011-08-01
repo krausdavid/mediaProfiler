5cd86d916b10b3f1e9ff6de285eb87def5d00038
David Kraus <krausdavid@informatik.uni-marburg.de>
2011-08-01 12:52:45 +0200

    Add MP_OB_date
    start creating constructor

--- src/objekts/MP_OB_mediaObject.java
+++ src/objekts/MP_OB_mediaObject.java
@@ -2,6 +2,8 @@ package objekts;
 
 
 import enums.Kind;
+
+import javax.print.attribute.standard.Media;
 import java.util.ArrayList;
 
 /**
@@ -10,6 +12,7 @@ import java.util.ArrayList;
  * Date: 24.07.11
  * Time: 22:45
  * To change this template use File | Settings | File Templates.
+ * -1 is the null for integer
  */
 public class MP_OB_mediaObject {
 
@@ -35,4 +38,50 @@ public class MP_OB_mediaObject {
 
 
 //######By Books######
+
+
+    public MP_OB_mediaObject(Kind kind, long isbn, String name, ArrayList<String> actors, String director, String producer, String format, String language, String subtitles, String studio, MP_OB_date erscheinung, int year_of_production, int time, int age, String kurzbeschreibung) {
+
+        this.kind = kind;
+        this.isbn = isbn;
+        this.name = name;
+        this.kurzbeschreibung = kurzbeschreibung;
+
+
+        if(kind == Kind.DVD || kind == Kind.BLUERAY){
+            this.actors = actors;
+            this.director = director;
+            this.producer = producer;
+            this.format = format;
+            this.language = language;
+            this.subtitles = subtitles;
+            this.studio = studio;
+            this.erscheinung = erscheinung;
+            this.year_of_production = year_of_production;
+            this.time = time;
+            this.age = age;
+        }else{
+            this.actors = null;
+            this.director = null;
+            this.producer = null;
+            this.format = null;
+            this.language = null;
+            this.subtitles = null;
+            this.studio = null;
+            this.erscheinung = null;
+            this.year_of_production = -1;
+            this.time = -1;
+            this.age = -1;
+
+        }
+
+        if(kind == Kind.BOOK){
+
+        }
+
+        if(kind == Kind.CD){
+
+        }
+
+    }
 }
